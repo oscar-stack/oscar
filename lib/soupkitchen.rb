d549ff6477114514fa369889c177241fcfa5d29d
Adrien Thebo <adrien@puppetlabs.com>
2012-08-23 10:58:14 -0700

    Add more values for soupkitchen config

--- lib/soupkitchen.rb
+++ lib/soupkitchen.rb
@@ -1,3 +1,8 @@
 module SoupKitchen; end
 
 require 'soupkitchen/config'
+require 'soupkitchen/command'
+require 'vagrant/config/pe'
+
+Vagrant.config_keys.register(:soupkitchen)  { Vagrant::Config::SoupKitchen }
+Vagrant.commands.register(:soupkitchen)     { SoupKitchen::Command }
--- lib/vagrant/config/pe.rb
+++ lib/vagrant/config/pe.rb
@@ -3,9 +3,19 @@ require 'uri'
 
 class Vagrant::Config::SoupKitchen < Vagrant::Config::Base
   attr_writer :download_root
+  attr_writer :default_version
+  attr_writer :filename
 
   def download_root
     @download_root || 'https://pm.puppetlabs.com/puppet-enterprise'
+  end
+
+  def default_version
+    @default_version || '2.5.3'
+  end
+
+  def filename
+    @filename || 'puppet-enterprise-2.5.3-all.tar.gz'
   end
 
   def validate(env, errors)
@@ -15,5 +25,3 @@ class Vagrant::Config::SoupKitchen < Vagrant::Config::Base
     errors.add("Invalid download root for Puppet Enterprise")
   end
 end
-
-Vagrant.config_keys.register(:soupkitchen) { Vagrant::Config::SoupKitchen }
