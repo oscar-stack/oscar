07954151200b5fcb5c67593ac5456f33e744e79e
Adrien Thebo <adrien@puppetlabs.com>
2012-08-24 09:23:48 -0700

    Subcommands

--- lib/pe_build/command.rb
+++ lib/pe_build/command.rb
@@ -2,14 +2,25 @@ require 'pe_build'
 require 'vagrant'
 
 class PEBuild::Command < Vagrant::Command::Base
+  def initialize(argv, env)
+    @argv, @env = argv, env
+
+    @main_args, @subcommand, @sub_args = split_main_and_subcommand(argv)
+
+    # Is this even remotely sane? Are we verging on cargo cult programming?
+    @subcommands = Vagrant::Registry.new
+
+    @subcommands.register('download') { PEBuild::Command::Download }
+    @subcommands.register('list')     { PEBuild::Command::List }
+  end
+
   def execute
-    basedir = "#{@env.home_path}/soupkitchen"
-    if File.directory? basedir
-      Dir["#{@env.home_path}/soupkitchen/*"].each do |path|
-        puts path
-      end
+    if @subcommand and (klass = @subcommands.get(@subcommand))
+      klass.new(@argv, @env).execute
+    elsif @subcommand
+      raise "Unrecognized subcommand #{@subcommand}"
     else
-      warn "No PE versions downloaded."
+      PEBuild::Command::List.new(@argv, @env).execute
     end
   end
 end
