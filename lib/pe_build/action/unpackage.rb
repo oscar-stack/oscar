f24e123923e787dca81b458f0cb0f8047b811106
Adrien Thebo <adrien@puppetlabs.com>
2012-08-28 16:42:31 -0700

    Add conditional extraction and idempotence to unpackaging

--- lib/pe_build/action/unpackage.rb
+++ lib/pe_build/action/unpackage.rb
@@ -6,6 +6,7 @@ class PEBuild::Action::Unpackage
   def initialize(app, env)
     @app, @env = app, env
     load_variables
+    @extracted_dir = File.join(@env[:unpack_directory], destination_directory)
   end
 
   def call(env)
@@ -13,6 +14,8 @@ class PEBuild::Action::Unpackage
     extract_build
     @app.call(@env)
   end
+
+  private
 
   def load_variables
     if @env[:box_name]
@@ -31,8 +34,28 @@ class PEBuild::Action::Unpackage
   # Sadly, shelling out is more sane than trying to use the facilities
   # provided.
   def extract_build
-    cmd = %{tar xf #{@archive_path} -C #{@env[:unpack_directory]}}
-    @env[:ui].info "Executing \"#{cmd}\""
-    %x{#{cmd}}
+    if File.directory? @extracted_dir
+      @env[:ui].info "#{@extracted_dir} already present, skipping extraction."
+    else
+      cmd = %{tar xf #{@archive_path} -C #{@env[:unpack_directory]}}
+      @env[:ui].info "Executing \"#{cmd}\""
+      %x{#{cmd}}
+    end
+  rescue => e
+    # If anything goes wrong while extracting, nuke the extracted directory
+    # as it could be incomplete. If we do this, then we can ensure that if
+    # the extracted directory already exists then it will be in a good state.
+    FileUtils.rm_r @extracted_dir
+  end
+
+  # Determine the name of the top level directory by peeking into the tarball
+  def destination_directory
+    out = IO.popen %{tar -tf #{@archive_path}}
+    firstline = out.gets
+    if (match = firstline.match %r[^(.*?)/])
+      match[1]
+    end
+  ensure
+    out.close
   end
 end
