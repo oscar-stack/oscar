7df8e0f8a12ef6ecc139472968e775475482c16f
Adrien Thebo <git@somethingsinistral.net>
2013-08-18 20:08:18 -0700

    [runner] Add explicit requires for deps
    Vagrant automatically requires plugins that have been installed, but it
    does not require any of the gem dependencies. We need to manually load
    them ourselves when using Oscar.

--- lib/oscar/runner.rb
+++ lib/oscar/runner.rb
@@ -1,8 +1,12 @@
-require 'config_builder'
 
 module Oscar
   module Runner
     def run(config_dir)
+      require 'vagrant-hosts'
+      require 'vagrant-pe_build'
+      require 'vagrant-auto_network'
+      require 'vagrant-config_builder'
+
       ConfigBuilder.load(:yaml, :yamldir, config_dir)
     end
   end
